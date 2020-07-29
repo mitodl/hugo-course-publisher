ab552a6f8105600807d363c4fbfc9cb0f48fc3ad
Carey Gumaer <gumaer.carey@gmail.com>
2020-07-29 14:14:16 -0400

    typo in webpack command

--- build-scripts/start_single_course.sh
+++ build-scripts/start_single_course.sh
@@ -3,7 +3,7 @@
 if [ "$1" != "" ]; then
   trap 'kill 0' EXIT
   hugo server -s site -p 3000 --bind 0.0.0.0 --theme single_course --contentDir $1 &
-  npm run start:webpack &
+  npm run start:multi:webpack &
   wait
 else
   echo "Please provide a course ID"
