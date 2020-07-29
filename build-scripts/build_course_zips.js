2b3736b6c3bdd35ab3c2879908a000726eb50eb5
Carey Gumaer <gumaer.carey@gmail.com>
2020-07-29 19:08:03 -0400

    working proof of concept for generating zips of a working site for each course

--- build-scripts/build_course_zips.js
+++ build-scripts/build_course_zips.js
@@ -1,31 +1,30 @@
 const fs = require("fs")
 const walk = require("walk")
 const tmp = require("tmp")
+tmp.setGracefulCleanup()
 const rimraf = require("rimraf")
 const archiver = require("archiver")
 const path = require("path")
-// const AWS = require("aws-sdk")
-// const env = require("dotenv").config().parsed
 const cliProgress = require("cli-progress")
+const shell = require("shelljs")
 
 const { directoryExists } = require("../src/js/helpers")
 
 const distPath = "dist"
-const coursesPath = path.join(distPath, "courses")
+const coursesPath = path.join("site", "content", "courses")
 const zipsPath = "zips"
 
 rimraf.sync(zipsPath)
 fs.mkdirSync(zipsPath, { recursive: true })
 
 if (directoryExists(distPath) && directoryExists(coursesPath)) {
-  let baseSiteFiles = []
-  // walk the built site path, filtering out course specific files
+  let webpackFiles = []
+  // gather files from the webpack build
   walk.walk(distPath, {
-    filters:   [coursesPath],
     listeners: {
       file: (root, fileStats, next) => {
         // add each file to a list
-        baseSiteFiles.push({
+        webpackFiles.push({
           root: root,
           name: fileStats.name
         })
@@ -33,7 +32,7 @@ if (directoryExists(distPath) && directoryExists(coursesPath)) {
       },
       end: () => {
         // filter out hidden files
-        baseSiteFiles = baseSiteFiles.filter(file => !file.name.startsWith("."))
+        webpackFiles = webpackFiles.filter(file => !file.name.startsWith("."))
         const progressBar = new cliProgress.SingleBar(
           { stopOnComplete: true },
           cliProgress.Presets.shades_classic
@@ -44,29 +43,52 @@ if (directoryExists(distPath) && directoryExists(coursesPath)) {
           .filter(course => !course.includes("."))
         progressBar.start(courses.length, 0)
         courses.forEach(course => {
-          // create the archive
-          const archive = archiver("zip")
-          // add the base site files
-          baseSiteFiles.forEach(file => {
-            archive.file(path.join(file.root, file.name))
-          })
-          // add the course files
-          walk.walk(path.join(coursesPath, course), {
-            listeners: {
-              file: (root, fileStats, next) => {
-                archive.file(path.join(root, fileStats.name))
-                next()
-              },
-              end: () => {
-                const output = fs.createWriteStream(
-                  path.join(zipsPath, `${course}.zip`)
+          // run the hugo build
+          const tmpDir = tmp.dirSync({
+            prefix: "dist"
+          }).name
+          tmpDir.setGr
+          if (
+            shell.exec(
+              `hugo -d ${tmpDir} -s site --theme single_course --contentDir ${path.join(
+                "..",
+                coursesPath,
+                course
+              )} --quiet`
+            ).code === 0
+          ) {
+            const files = fs.readdirSync(tmpDir)
+            // create the archive
+            const archive = archiver("zip")
+            // add the webpack files
+            webpackFiles.forEach(file => {
+              archive.file(path.join(file.root, file.name), {
+                name: path.join(
+                  file.root.replace(new RegExp(`${distPath}/?`, "g"), ""),
+                  file.name
                 )
-                archive.pipe(output)
-                archive.finalize()
-                progressBar.increment()
+              })
+            })
+            // add the course files
+            walk.walk(tmpDir, {
+              listeners: {
+                file: (root, fileStats, next) => {
+                  archive.file(path.join(root, fileStats.name), {
+                    name: path.join(root.replace(tmpDir, ""), fileStats.name)
+                  })
+                  next()
+                },
+                end: () => {
+                  const output = fs.createWriteStream(
+                    path.join(zipsPath, `${course}.zip`)
+                  )
+                  archive.pipe(output)
+                  archive.finalize()
+                  progressBar.increment()
+                }
               }
-            }
-          })
+            })
+          }
         })
       }
     }
