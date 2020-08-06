b695744edb7cd9c608fdb85b5a13736398c8f557
Carey Gumaer <gumaer.carey@gmail.com>
2020-08-06 16:18:28 -0400

    prepend start_single_course argument with ../ so that the path you pass in is relative to your cwd

--- build-scripts/start_single_course.sh
+++ build-scripts/start_single_course.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" != "" ]; then
   trap 'kill 0' EXIT
-  hugo server -s site -p 3000 --bind 0.0.0.0 --theme single_course --contentDir $1 &
+  hugo server -s site -p 3000 --bind 0.0.0.0 --theme single_course --contentDir ../$1 &
   npm run start:multi:webpack &
   wait
 else
