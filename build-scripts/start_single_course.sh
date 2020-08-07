57ef9708de368804a26e3a01b24292166cd72716
Carey Gumaer <gumaerc@mit.edu>
2020-08-07 11:20:49 -0400

    check in the start single course script if argument is an absolute or relative path and act accordingly

--- build-scripts/start_single_course.sh
+++ build-scripts/start_single_course.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-if [ "$1" != "" ]; then
+if [ `expr index "$1" '/'` != 1 ]
+then
+  CONTENT_DIR="../$1"
+else
+  CONTENT_DIR="$1"
+fi
+
+if [ "$CONTENT_DIR" != "" ]; then
   trap 'kill 0' EXIT
-  hugo server -s site -p 3000 --bind 0.0.0.0 --theme single_course --contentDir ../$1 &
+  echo $PWD
+  hugo server -s site -p 3000 --bind 0.0.0.0 --theme single_course --contentDir $CONTENT_DIR &
   npm run start:multi:webpack &
   wait
 else
