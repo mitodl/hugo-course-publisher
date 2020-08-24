f9bf082ca0c6959993121f4b0bbe6fbc38a70cfe
Carey Gumaer <gumaer.carey@gmail.com>
2020-08-24 17:41:15 -0400

    restructure uploader to replace the contents of the bucket with the built site every time, and remove progress bar stuff

--- build-scripts/s3_upload.js
+++ build-scripts/s3_upload.js
@@ -7,13 +7,6 @@ tmp.setGracefulCleanup()
 const path = require("path")
 require("dotenv").config()
 
-const { newProgressBar } = require("../src/js/helpers")
-
-// init progress bar
-const progressBar = newProgressBar()
-let totalFiles = 0
-let uploadedFiles = 0
-
 // get the path of the built site
 const options = yargs.usage("Usage: -d <dist>").option("d", {
   alias:        "dist",
@@ -46,40 +39,68 @@ const handleUploadResult = (err, data) => {
   if (err) {
     console.error(err)
   }
-  if (data) {
-    uploadedFiles++
-    progressBar.update(uploadedFiles)
-  }
+  console.log(data)
 }
 
-// upload the site
+// configure aws-sdk
 AWS.config = new AWS.Credentials({
   accessKeyId:     process.env.AWS_ACCESS_KEY,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 })
 const s3 = new AWS.S3()
-const now = Date.now().toString()
-progressBar.start(0, 0)
-walk.walk(distPath, {
-  listeners: {
-    file: (root, fileStats, next) => {
-      totalFiles++
-      progressBar.setTotal(totalFiles)
-      const filePath = path.join(root, fileStats.name)
-      const fileStream = fs.createReadStream(filePath)
-      fileStream.on("error", handleError)
-      const awsKey = path.join(
-        now,
-        root.replace(new RegExp(`${distPath}/?`), ""),
-        fileStats.name
-      )
-      const uploadParams = {
-        Bucket: process.env.AWS_STORAGE_BUCKET_NAME,
-        Key:    awsKey,
-        Body:   fileStream
-      }
-      s3.upload(uploadParams, handleUploadResult)
-      next()
+const bucketParams = { Bucket: process.env.AWS_STORAGE_BUCKET_NAME }
+
+// define s3 functions
+
+const deleteAllObjectsFromS3Bucket = async (s3, params) => {
+  const data = await s3.listObjectsV2(params).promise()
+  const objectCount = data.Contents.length
+  const deleteParams = {
+    Bucket: params.Bucket,
+    Delete: {
+      Objects: data.Contents.map(o => ({ Key: o.Key })),
+      Quiet:   false
     }
   }
+  if (objectCount > 0) {
+    await s3.deleteObjects(deleteParams).promise()
+    if (data.IsTruncated && data.NextContinuationToken) {
+      params.ContinuationToken = data.NextContinuationToken
+      await deleteAllObjectsFromS3Bucket(s3, params)
+    }
+  }
+}
+
+const uploadSite = async (s3, params) => {
+  console.log("Clearing bucket...")
+  await deleteAllObjectsFromS3Bucket(s3, params)
+  walk.walk(distPath, {
+    listeners: {
+      file: async (root, fileStats, next) => {
+        const filePath = path.join(root, fileStats.name)
+        const fileStream = fs.createReadStream(filePath)
+        fileStream.on("error", handleError)
+        const awsKey = path.join(
+          root.replace(new RegExp(`${distPath}/?`), ""),
+          fileStats.name
+        )
+        const uploadParams = {
+          Bucket: params.Bucket,
+          Key:    awsKey,
+          Body:   fileStream
+        }
+        await s3.upload(uploadParams).promise()
+        console.log(`Uploaded ${awsKey}`)
+        next()
+      },
+      end: () => {
+        console.log("Upload Complete")
+      }
+    }
+  })
+}
+
+// upload the site
+deleteAllObjectsFromS3Bucket(s3, bucketParams).then(() => {
+  uploadSite(s3, bucketParams)
 })
