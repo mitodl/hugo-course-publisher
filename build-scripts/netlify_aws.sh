fb566587fd39b2e4fae918e2feef3f5f5e1a815c
Carey Gumaer <gumaerc@mit.edu>
2020-09-04 19:08:43 -0400

    try just the creds

--- build-scripts/netlify_aws.sh
+++ build-scripts/netlify_aws.sh
@@ -1,8 +1,4 @@
 mkdir ~/.aws
-cat > ~/.aws/config <<EOL
-[profile default]
-region = $ENV_DEFAULT_REGION
-EOL
 cat > ~/.aws/credentials <<EOL
 [profile default]
 aws_secret_access_key = $ENV_SECRET_ACCESS_KEY
