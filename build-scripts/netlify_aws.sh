93e77c573c0dbbe738f370e73ef8aef0f2ad977e
Carey Gumaer <gumaerc@mit.edu>
2020-09-04 18:49:23 -0400

    try putting credentials in the aws config file and use the default profile

--- build-scripts/netlify_aws.sh
+++ build-scripts/netlify_aws.sh
@@ -1,6 +1,6 @@
 mkdir ~/.aws
-cat > ~/.aws/credentials <<EOL
-[netlify]
+cat > ~/.aws/config <<EOL
+[profile default]
 region = $ENV_DEFAULT_REGION
 aws_secret_access_key = $ENV_SECRET_ACCESS_KEY
 aws_access_key_id = $ENV_ACCESS_KEY_ID
