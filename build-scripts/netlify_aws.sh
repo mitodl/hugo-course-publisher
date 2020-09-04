1f6aa54c6396deef3281edb6cc3dae8f921dba52
Carey Gumaer <gumaerc@mit.edu>
2020-09-04 19:37:44 -0400

    back to this I guess

--- build-scripts/netlify_aws.sh
+++ build-scripts/netlify_aws.sh
@@ -2,6 +2,9 @@ mkdir ~/.aws
 cat > ~/.aws/config <<EOL
 [default]
 region = $ENV_DEFAULT_REGION
+EOL
+cat > ~/.aws/credentials <<EOL
+[default]
 aws_secret_access_key = $ENV_SECRET_ACCESS_KEY
 aws_access_key_id = $ENV_ACCESS_KEY_ID
 EOL
