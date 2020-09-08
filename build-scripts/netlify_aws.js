9775ecfef5d4a162897594a6d4b88f1356104403
Carey Gumaer <gumaerc@mit.edu>
2020-09-08 13:09:46 -0400

    try using the variables directly to download courses

--- build-scripts/netlify_aws.js
+++ build-scripts/netlify_aws.js
@@ -1,12 +1,28 @@
-const shell = require("shelljs")
+const AWS = require("aws-sdk")
+const fs = require("fs")
+const { downloadCourseRecursive } = require("@mitodl/ocw-to-hugo")
 require("dotenv").config()
 
 const awsRegion = process.env["ENV_DEFAULT_REGION"]
 const accessKeyId = process.env["ENV_ACCESS_KEY"]
 const secretAccessKey = process.env["ENV_SECRET_ACCESS_KEY"]
+AWS.config = new AWS.Config({
+  region:      awsRegion,
+  credentials: new AWS.Credentials({
+    accessKeyId:     accessKeyId,
+    secretAccessKey: secretAccessKey
+  })
+})
 
-shell.exec(`export AWS_REGION=${awsRegion}`)
-shell.exec(`export AWS_ACCESS_KEY_ID=${accessKeyId}`)
-shell.exec(`export AWS_SECRET_ACCESS_KEY=${secretAccessKey}`)
-
-console.log(`aws region: ${awsRegion}`)
+const s3 = new AWS.S3()
+const courses = JSON.parse(fs.readFileSync("example_courses.json"))["courses"]
+const totalCourses = courses.length
+console.log(`Downloading ${totalCourses} courses from AWS...`)
+courses.map(async course => {
+  const bucketParams = {
+    Bucket: process.env["AWS_BUCKET_NAME"],
+    Prefix: course
+  }
+  await downloadCourseRecursive(s3, bucketParams, "private/courses")
+  console.log(`${course} downloaded...`)
+})
