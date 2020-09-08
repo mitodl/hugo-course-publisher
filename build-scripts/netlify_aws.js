7b702761550afb47d1fc3af0d2d25dcb178f5d34
Carey Gumaer <gumaerc@mit.edu>
2020-09-08 12:35:47 -0400

    hopefully this should work

--- build-scripts/netlify_aws.js
+++ build-scripts/netlify_aws.js
@@ -1,9 +1,22 @@
 const fs = require("fs")
 const path = require("path")
+const homeDir = require("os").homedir()
 require("dotenv").config()
+
+const helpers = require("../src/js/helpers")
 
 const awsRegion = process.env["ENV_DEFAULT_REGION"]
 const accessKey = process.env["ENV_ACCESS_KEY"]
 const secretAccessKey = process.env["ENV_SECRET_ACCESS_KEY"]
-fs.writeFileSync(path.join("~", ".aws", "config"), `[default]\nregion=${awsRegion}\noutput=json`)
-fs.writeFileSync(path.join("~", ".aws", "credentials"), `[default]\naws_access_key_id=${accessKey}\naws_secret_access_Key=${secretAccessKey}`)
+const awsDir = path.join(homeDir, ".aws")
+if (!helpers.directoryExists(awsDir)) {
+  fs.mkdirSync(awsDir, { recursive: true })
+}
+fs.writeFileSync(
+  path.join(awsDir, "config"),
+  `[default]\nregion=${awsRegion}\noutput=json\n`
+)
+fs.writeFileSync(
+  path.join(awsDir, "credentials"),
+  `[default]\naws_access_key_id=${accessKey}\naws_secret_access_Key=${secretAccessKey}\n`
+)
