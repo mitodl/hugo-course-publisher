759c38d77802a7804834813b6c58b5ec38e41734
George Schneeloch <gschneel@mit.edu>
2020-10-13 18:00:58 -0400

    Test

--- build-scripts/build_course_zips.test.js
+++ build-scripts/build_course_zips.test.js
@@ -1,48 +1,54 @@
 const path = require("path")
-const rimraf = require("rimraf")
-const tmp = require("tmp")
+const fsPromises = require("fs").promises
 
 const { buildZips } = require("./build_course_zips")
 
 describe("build_course_zips", () => {
   const cwd = process.cwd()
   const distPath = path.join(cwd, "dist")
-  const coursesPath = path.join(cwd, "site", "content", "courses")
   const zipsError = new Error(
     "zips path must not be within hugo content or static directories"
   )
 
   it("throws an error if the coursesPath does not exist", async () => {
-    const invalidCoursesPath = tmp.dirSync({
-      prefix: "courses"
-    }).name
-    rimraf.sync(invalidCoursesPath)
-    await buildZips(distPath, invalidCoursesPath).catch(err => {
-      expect(err).toEqual(
-        new Error(`Courses path "${invalidCoursesPath}" not found`)
-      )
-    })
+    let currentCwd
+    try {
+      currentCwd = process.cwd()
+      process.chdir("/tmp")
+      await buildZips(distPath).catch(err => {
+        expect(err).toEqual(
+          new Error(`Courses path "/tmp/site/content/courses" not found`)
+        )
+      })
+    } finally {
+      process.chdir(currentCwd)
+    }
   })
 
   it("throws an error if you pass it an empty courses directory", async () => {
-    const invalidCoursesPath = tmp.dirSync({
-      prefix: "courses"
-    }).name
-    await buildZips(distPath, invalidCoursesPath).catch(err => {
-      expect(err).toEqual(new Error("No courses found"))
-    })
+    let currentCwd
+    try {
+      currentCwd = process.cwd()
+      process.chdir("/tmp")
+      await fsPromises.mkdir("/tmp/site/content/courses", { recursive: true })
+      await buildZips(distPath).catch(err => {
+        expect(err).toEqual(new Error("No courses found"))
+      })
+    } finally {
+      process.chdir(currentCwd)
+    }
   })
 
   it("throws an error when called with content dir as zip destination", async () => {
     const invalidZipsPath = path.join(cwd, "site", "content")
-    await buildZips(distPath, coursesPath, invalidZipsPath).catch(err => {
+    await buildZips(distPath, invalidZipsPath).catch(err => {
       expect(err).toEqual(zipsError)
     })
   })
 
   it("throws an error when called with static dir as zip destination", async () => {
     const invalidZipsPath = path.join(cwd, "site", "static")
-    await buildZips(distPath, coursesPath, invalidZipsPath).catch(err => {
+    await buildZips(distPath, invalidZipsPath).catch(err => {
       expect(err).toEqual(zipsError)
     })
   })
