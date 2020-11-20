24c36181afbae1e2068d9fe3079cdabbfed395ef
George Schneeloch <gschneel@mit.edu>
2020-11-20 18:14:35 -0500

    Add read more link for course description

--- src/js/course_expander.js
+++ src/js/course_expander.js
@@ -1,3 +1,31 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import ClampLines from "react-clamp-lines"
+
+class ModifiedClampLines extends ClampLines {
+  render() {
+    if (!this.props.text) {
+      return null
+    }
+
+    return (
+      <div className={this.getClassName()}>
+        <div
+          ref={e => (this.element = e)}
+          id={`clamped-content-${this.uuid}`}
+          aria-hidden={this.state.expanded}
+        >
+          {this.watch ? this.state.text : null}
+        </div>
+        {this.watch ? null : (
+          <div dangerouslySetInnerHTML={{ __html: this.props.originalHtml }} />
+        )}
+        {this.getButton()}
+      </div>
+    )
+  }
+}
+
 const toggleExpand = (button, container) => {
   const expanded = button.classList.contains("expanded")
   if (expanded) {
@@ -26,6 +54,34 @@ const initCourseInfoExpander = () => {
         }
       })
     }
+  }
+
+  let index = 0
+  for (const container of document.querySelectorAll(".course-description")) {
+    const html = container.innerHTML
+    const text = container.innerText
+    container.innerHTML = ""
+    ReactDOM.render(
+      <ModifiedClampLines
+        originalHtml={html}
+        text={text}
+        id={`course-description-${index}`}
+        moreText={
+          <span>
+            Read more <i className="material-icons">keyboard_arrow_right</i>
+          </span>
+        }
+        lessText={
+          <span>
+            Show Less <i className="material-icons">keyboard_arrow_down</i>
+          </span>
+        }
+        lines={5}
+      />,
+      container
+    )
+
+    index += 1
   }
 }
 
