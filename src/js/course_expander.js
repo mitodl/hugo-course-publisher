4186df8a64f5e32ec1ea68e764cf436247a3e6a3
George Schneeloch <gschneel@mit.edu>
2020-11-23 18:19:32 -0500

    WIP

--- src/js/course_expander.js
+++ src/js/course_expander.js
@@ -1,33 +1,6 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import ClampLines from "react-clamp-lines"
-
-class ModifiedClampLines extends ClampLines {
-  render() {
-    if (!this.props.text) {
-      return null
-    }
-
-    return (
-      <div className={this.getClassName()}>
-        <div
-          ref={e => (this.element = e)}
-          id={`clamped-content-${this.uuid}`}
-          aria-hidden={this.state.expanded}
-        >
-          {this.watch ? this.state.text : null}
-        </div>
-        {this.watch ? null : (
-          <div dangerouslySetInnerHTML={{ __html: this.props.originalHtml }} />
-        )}
-        {this.getButton()}
-      </div>
-    )
-  }
-}
-
 const toggleExpand = (button, container) => {
   const expanded = button.classList.contains("expanded")
+  const text = button.querySelector(".text")
   if (expanded) {
     button.classList.remove("expanded")
     container.classList.add("collapsed")
@@ -36,6 +9,10 @@ const toggleExpand = (button, container) => {
     button.classList.add("expanded")
     container.classList.remove("collapsed")
     button.querySelector(".material-icons").textContent = "keyboard_arrow_down"
+  }
+
+  if (text) {
+    text.innerText = expanded ? "Read More" : "Show Less"
   }
 }
 
@@ -56,32 +33,21 @@ const initCourseInfoExpander = () => {
     }
   }
 
-  let index = 0
   for (const container of document.querySelectorAll(".course-description")) {
-    const html = container.innerHTML
-    const text = container.innerText
-    container.innerHTML = ""
-    ReactDOM.render(
-      <ModifiedClampLines
-        originalHtml={html}
-        text={text}
-        id={`course-description-${index}`}
-        moreText={
-          <span>
-            Read More <i className="material-icons">keyboard_arrow_right</i>
-          </span>
+    const expanderButton = container.querySelector(".expand-link")
+    console.log("cont", container, expanderButton)
+    if (expanderButton) {
+      expanderButton.addEventListener("click", event => {
+        event.preventDefault()
+        toggleExpand(expanderButton, container)
+      })
+      expanderButton.addEventListener("keypress", event => {
+        if (event.key === "Enter") {
+          event.preventDefault()
+          toggleExpand(expanderButton, container)
         }
-        lessText={
-          <span>
-            Show Less <i className="material-icons">keyboard_arrow_down</i>
-          </span>
-        }
-        lines={5}
-      />,
-      container
-    )
-
-    index += 1
+      })
+    }
   }
 }
 
