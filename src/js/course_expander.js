165993d57f8b7134cda2cbc85c66db7822755c40
George Schneeloch <gschneel@mit.edu>
2020-11-23 12:07:34 -0500

    Implement collapse/expand for course info (#396)
    * Implement collapse/expand for course info
    
    * Don't show collapse for side panel
    
    * Fix link
    
    * Add course info as part of expander link
    
    * Shorten collapse height to not show departments

---
+++ src/js/course_expander.js
@@ -0,0 +1,32 @@
+const toggleExpand = (button, container) => {
+  const expanded = button.classList.contains("expanded")
+  if (expanded) {
+    button.classList.remove("expanded")
+    container.classList.add("collapsed")
+    button.querySelector(".material-icons").textContent = "keyboard_arrow_right"
+  } else {
+    button.classList.add("expanded")
+    container.classList.remove("collapsed")
+    button.querySelector(".material-icons").textContent = "keyboard_arrow_down"
+  }
+}
+
+const initCourseInfoExpander = () => {
+  for (const container of document.querySelectorAll(".course-info")) {
+    const expanderButton = container.querySelector(".expand-link")
+    if (expanderButton) {
+      expanderButton.addEventListener("click", event => {
+        event.preventDefault()
+        toggleExpand(expanderButton, container)
+      })
+      expanderButton.addEventListener("keypress", event => {
+        if (event.key === "Enter") {
+          event.preventDefault()
+          toggleExpand(expanderButton, container)
+        }
+      })
+    }
+  }
+}
+
+export { initCourseInfoExpander }
