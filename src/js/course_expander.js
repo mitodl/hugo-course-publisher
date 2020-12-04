38a97106d4f3765a659bb8594554c58a0138023b
George Schneeloch <gschneel@mit.edu>
2020-12-04 10:19:26 -0500

    Add read more link for course description (#410)

--- src/js/course_expander.js
+++ src/js/course_expander.js
@@ -1,5 +1,6 @@
 const toggleExpand = (button, container) => {
   const expanded = button.classList.contains("expanded")
+  const text = button.querySelector(".text")
   if (expanded) {
     button.classList.remove("expanded")
     container.classList.add("collapsed")
@@ -9,11 +10,32 @@ const toggleExpand = (button, container) => {
     container.classList.remove("collapsed")
     button.querySelector(".material-icons").textContent = "keyboard_arrow_down"
   }
+
+  if (text) {
+    text.innerText = expanded ? "Read More" : "Show Less"
+  }
 }
 
 const initCourseInfoExpander = () => {
   for (const container of document.querySelectorAll(".course-info")) {
     const expanderButton = container.querySelector(".expand-link")
+    if (expanderButton) {
+      expanderButton.addEventListener("click", event => {
+        event.preventDefault()
+        toggleExpand(expanderButton, container)
+      })
+      expanderButton.addEventListener("keypress", event => {
+        if (event.key === "Enter") {
+          event.preventDefault()
+          toggleExpand(expanderButton, container)
+        }
+      })
+    }
+  }
+
+  for (const container of document.querySelectorAll(".course-description")) {
+    const expanderButton = container.querySelector(".expand-link")
+    console.log("cont", container, expanderButton)
     if (expanderButton) {
       expanderButton.addEventListener("click", event => {
         event.preventDefault()
